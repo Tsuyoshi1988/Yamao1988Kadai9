1b80d3c96a6880f439de2384f6a9c7da27389d68
akio0911 <akio0911@gmail.com>
2023-12-06 21:21:34 +0900

    feedback

--- kadai9Pre/YellowViewController.swift
+++ kadai9Pre/YellowViewController.swift
@@ -4,10 +4,4 @@ import UIKit
 class YellowViewController: UIViewController {
 
    @IBOutlet weak var YellowText: UITextField!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
 }
